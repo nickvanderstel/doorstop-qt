57aa6612c576d89a7f309a7a6ef630645070b296
nickvanderstel <njvander@gmail.com>
2016-12-05 00:19:16 -0500

    open project working, added working document tree

--- doorstop-qt.py
+++ doorstop-qt.py
@@ -1,14 +1,59 @@
 #!/usr/bin/python3
 
 """
-Doorstop GUI
+Doorstop PyQt GUI
 """
 
 
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QWidget
-from PyQt5.QtWidgets import QSplitter, QVBoxLayout, QTreeWidget, QTreeWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QWidget, QFileDialog
+from PyQt5.QtWidgets import QSplitter, QVBoxLayout, QTreeWidget, QTreeWidgetItem, QTableWidget
+from PyQt5.QtWidgets import QTabWidget
 from PyQt5.QtGui import QIcon
+
+from doorstop.core import builder
+
+
+
+class DocTreeItem(QTreeWidgetItem):
+    def __init__(self, doc):
+        super().__init__()
+        self.doc = doc
+
+class DocTree(QTreeWidget):
+
+    def __init__(self):
+        super().__init__()
+        self._docList = []
+        self.header().hide() # we don't need the header
+        #self.setHeaderLabels(['Requirement Tree'])
+
+    def addItem(self, doc):
+        if doc.parent is None:
+            self._addRootItem(doc)
+        else:
+            self._addChildItem(self._findParent(doc), doc)
+
+    def _addRootItem(self, doc):
+        rootItem = DocTreeItem(doc)
+        rootItem.setText(0, doc.prefix)
+        self.addTopLevelItem(rootItem)
+        self._docList.append(rootItem)
+
+    def _addChildItem(self, parent, doc):
+        childItem = DocTreeItem(doc)
+        childItem.setText(0, doc.prefix)
+        if parent is None:
+            raise ValueError('Can\'t find document parent for {}'.format(doc.prefix))
+        parent.addChild(childItem)
+        parent.setExpanded(True) # TODO: make this optional?
+        self._docList.append(childItem)
+
+    def _findParent(self, child):
+        for parent in self._docList:
+            if parent.doc.prefix == child.parent:
+                return parent
+        return None
 
 
 
@@ -16,7 +61,7 @@ class ReqTree(QTreeWidget):
 
     def __init__(self):
         super().__init__()
-        self.setHeaderLabels(['Requirement Tree'])
+        self.header().hide() # we don't need the header
 
     def addRootItem(self, index, text):
         rootItem = QTreeWidgetItem()
@@ -29,7 +74,6 @@ class ReqTree(QTreeWidget):
         parent.insertChild(finalIndex-1, childItem)
 
     def addItem(self, index, text):
-        print('additem ' + str(index))
         indexArray = [int(x) for x in index.split('.') if x.strip()]
         if not indexArray:
             raise ValueError('Invalid item index: empty')
@@ -59,13 +103,17 @@ class MainWindow(QMainWindow):
 
     def __init__(self, app):
         super().__init__()
-        self.startUI(app)
+        self._startUI(app)
+        self._tree = None
 
 
-    def startUI(self, app):
-        openAction = QAction('&Open', self)
+    def _startUI(self, app):
+
+        # Create the menubar
+        openAction = QAction('&Open Project', self)
         openAction.setShortcut('Ctrl+O')
-        openAction.setStatusTip('Open Requirements')
+        openAction.setStatusTip('Open Project')
+        openAction.triggered.connect(self._openProject)
 
         saveAction = QAction('&Save', self)
         saveAction.setShortcut('Ctrl+S')
@@ -93,15 +141,37 @@ class MainWindow(QMainWindow):
         helpMenu = menuBar.addMenu('Help')
         helpMenu.addAction(aboutAction)
 
-        mainContainer = QWidget()
 
+        # Parent container to hold everything in the main window
+        mainContainer = QWidget()
         self.setCentralWidget(mainContainer)
 
-        reqTree = ReqTree()
-        reqView = QTreeWidget()
+        # Document tree view widget
+        self._docTree = DocTree()
+        docTreeWindow = QWidget()
+        docTreeLayout = QVBoxLayout()
+        docTreeLayout.addWidget(self._docTree)
+        docTreeWindow.setLayout(docTreeLayout)
+
+        # Requirement tree view widget
+        self._reqTree = ReqTree()
+        reqTreeWindow = QWidget()
+        reqTreeLayout = QVBoxLayout()
+        reqTreeLayout.addWidget(self._reqTree)
+        reqTreeWindow.setLayout(reqTreeLayout)
+
+        # Tab widget to hold the tree views
+        self._tabs = QTabWidget()
+        self._tabs.addTab(docTreeWindow, 'Documents')
+        self._tabs.addTab(reqTreeWindow, 'Requirements')
+
+
+        # Requirement table view widget
+        reqView = QTableWidget()
+
 
         mainSplit = QSplitter()
-        mainSplit.addWidget(reqTree)
+        mainSplit.addWidget(self._tabs)
         mainSplit.addWidget(reqView)
         mainSplit.setStretchFactor(0,1)
         mainSplit.setStretchFactor(1,3)
@@ -113,14 +183,14 @@ class MainWindow(QMainWindow):
         test2 = str(test1)
         test = str("1.1")
 
-        reqTree.addItem('1', 'Hardware')
-        reqTree.addItem('1.1', 'AXI')
-        reqTree.addItem('1.2', 'DMA')
-        reqTree.addItem('1.3', 'SERDES')
-        reqTree.addItem('2', 'Software')
-        reqTree.addItem('1.1.1', 'req1')
-        reqTree.addItem('1.1.2', 'req2')
-        reqTree.addItem('1.1.3', 'req3')
+        self._reqTree.addItem('1', 'Hardware')
+        self._reqTree.addItem('1.1', 'AXI')
+        self._reqTree.addItem('1.2', 'DMA')
+        self._reqTree.addItem('1.3', 'SERDES')
+        self._reqTree.addItem('2', 'Software')
+        self._reqTree.addItem('1.1.1', 'req1')
+        self._reqTree.addItem('1.1.2', 'req2')
+        self._reqTree.addItem('1.1.3', 'req3')
 
         sg = app.desktop().screenGeometry()
         wmax = sg.width()
@@ -130,6 +200,16 @@ class MainWindow(QMainWindow):
         self.statusBar().showMessage('No Requirements Loaded')
         self.show()
 
+    def _openProject(self):
+        dialog = QFileDialog(self)
+        dialog.setFileMode(QFileDialog.DirectoryOnly)
+        dialog.setOptions(QFileDialog.ShowDirsOnly)
+        if dialog.exec():
+            # TODO: this returns a list... should give user an error if they select multiple
+            dir = dialog.selectedFiles()[0]
+            self._tree = builder.build(root=dir)
+            for doc in self._tree:
+                self._docTree.addItem(doc)
 
 
 if __name__ == '__main__':
