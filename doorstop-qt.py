9151c7c32c8bf55c5052f6b88c021e111e2bedc8
nickvanderstel <njvander@gmail.com>
2016-12-06 18:09:01 -0500

    switched to panes and stacked widgets instead of tabs

--- doorstop-qt.py
+++ doorstop-qt.py
@@ -6,9 +6,10 @@ Doorstop PyQt GUI
 
 
 import sys
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QWidget, QFileDialog
 from PyQt5.QtWidgets import QSplitter, QVBoxLayout, QTreeWidget, QTreeWidgetItem, QTableWidget
-from PyQt5.QtWidgets import QTabWidget
+from PyQt5.QtWidgets import QTabWidget, QStackedWidget
 from PyQt5.QtGui import QIcon
 
 from doorstop.core import builder
@@ -57,45 +58,107 @@ class DocTree(QTreeWidget):
 
 
 
+
 class ReqTree(QTreeWidget):
 
-    def __init__(self):
+    def __init__(self, doc=None):
         super().__init__()
         self.header().hide() # we don't need the header
+        if doc:
+            self.loadDoc(doc)
 
-    def addRootItem(self, index, text):
+    def loadDoc(self, doc):
+        for item in doc.items:
+            self.addItem(item.level, item.text)
+
+    def addItem(self, level, text):
+        # strip off trailing zero
+        index = level.value[:-1] if level.value[-1] == 0 else level.value
+        if not index:
+            raise ValueError('Invalid item index: empty')
+        if (len(index) == 1) or (len(index) == 2 and index[1] == 0):
+            self._addRootItem(index[0], text)
+        else:
+            finalIndex = index[-1]
+            subIndex = index[:-1]
+            self._addChildItem(self._findParent(subIndex), finalIndex, str(level), text)
+
+    def _addRootItem(self, index, text):
         rootItem = QTreeWidgetItem()
         rootItem.setText(0, str(index) + ' ' + text)
         self.insertTopLevelItem(index-1, rootItem)
 
-    def addChildItem(self, parent, finalIndex, index, text):
+    def _addChildItem(self, parent, finalIndex, index, text):
         childItem = QTreeWidgetItem()
         childItem.setText(0, str(index) + ' ' + text)
         parent.insertChild(finalIndex-1, childItem)
 
-    def addItem(self, index, text):
-        indexArray = [int(x) for x in index.split('.') if x.strip()]
-        if not indexArray:
-            raise ValueError('Invalid item index: empty')
-        if len(indexArray) == 1:
-            self.addRootItem(indexArray[0], text)
-        else:
-            finalIndex = indexArray[-1]
-            subIndex = indexArray[:-1]
-            self.addChildItem(self.findParent(subIndex), finalIndex, index, text)
-
-    def findParent(self, subIndex):
+    def _findParent(self, subIndex):
         if not subIndex:
             raise ValueError('Invalid item index: empty')
         parent = self.topLevelItem(subIndex[0]-1)
         if not parent:
             print('root not found ' + str(subIndex[0]-1))
         while (parent and subIndex[1:]):
-            del subIndex[0]
-            parent = parent.child(subIndex[0]-1)
+            parent = parent.child(subIndex[1]-1)
+            subIndex = subIndex[1:]
         if not parent:
             raise ValueError('Invalid item index: parent not found')
         return parent
+
+
+
+
+class DocTable(QTableWidget):
+
+    def __init__(self, doc=None):
+        super().__init__()
+        if doc:
+            self.loadDoc(doc)
+
+    def loadDoc(self, doc):
+        #self.setColumnCount(
+        self.setRowCount(len(doc))
+        for item in doc.items:
+            self._addItem(item)
+
+    def _addItem(self, item):
+        pass
+
+
+
+
+class TreeStack(QStackedWidget):
+
+    def __init__(self):
+        super().__init__()
+        self._docList = {}
+
+    def addDoc(self, doc):
+        newDoc = ReqTree(doc)
+        self._docList[doc.prefix] = newDoc
+        self.addWidget(newDoc)
+
+    def makeDocActive(self, doc):
+        self.setCurrentWidget(self._docList[doc.prefix])
+
+
+
+class DocStack(QStackedWidget):
+
+    def __init__(self):
+        super().__init__()
+        self._docList = {}
+
+    def addDoc(self, doc):
+        newDoc = DocTable(doc)
+        self._docList[doc.prefix] = newDoc
+        self.addWidget(newDoc)
+
+    def makeDocActive(self, doc):
+        self.setCurrentWidget(self._docList[doc.prefix])
+
+
 
 
 
@@ -152,45 +215,45 @@ class MainWindow(QMainWindow):
         docTreeLayout = QVBoxLayout()
         docTreeLayout.addWidget(self._docTree)
         docTreeWindow.setLayout(docTreeLayout)
+        self._docTree.itemActivated.connect(self._docSelected)
 
         # Requirement tree view widget
-        self._reqTree = ReqTree()
+        self._reqStack = TreeStack()
         reqTreeWindow = QWidget()
         reqTreeLayout = QVBoxLayout()
-        reqTreeLayout.addWidget(self._reqTree)
+        reqTreeLayout.addWidget(self._reqStack)
         reqTreeWindow.setLayout(reqTreeLayout)
 
+        # splitter for the tree views
+        treeSplit = QSplitter()
+        treeSplit.setOrientation(Qt.Vertical)
+        treeSplit.addWidget(docTreeWindow)
+        treeSplit.addWidget(reqTreeWindow)
+        treeSplit.setStretchFactor(0,1)
+        treeSplit.setStretchFactor(1,9)
+
+
+        """
         # Tab widget to hold the tree views
         self._tabs = QTabWidget()
         self._tabs.addTab(docTreeWindow, 'Documents')
         self._tabs.addTab(reqTreeWindow, 'Requirements')
+        """
 
 
         # Requirement table view widget
-        reqView = QTableWidget()
+        self._reqView = DocStack()
 
 
         mainSplit = QSplitter()
-        mainSplit.addWidget(self._tabs)
-        mainSplit.addWidget(reqView)
+        #mainSplit.addWidget(self._tabs)
+        mainSplit.addWidget(treeSplit)
+        mainSplit.addWidget(self._reqView)
         mainSplit.setStretchFactor(0,1)
         mainSplit.setStretchFactor(1,3)
         layout = QVBoxLayout()
         layout.addWidget(mainSplit)
         mainContainer.setLayout(layout)
-
-        test1 = '1.1'
-        test2 = str(test1)
-        test = str("1.1")
-
-        self._reqTree.addItem('1', 'Hardware')
-        self._reqTree.addItem('1.1', 'AXI')
-        self._reqTree.addItem('1.2', 'DMA')
-        self._reqTree.addItem('1.3', 'SERDES')
-        self._reqTree.addItem('2', 'Software')
-        self._reqTree.addItem('1.1.1', 'req1')
-        self._reqTree.addItem('1.1.2', 'req2')
-        self._reqTree.addItem('1.1.3', 'req3')
 
         sg = app.desktop().screenGeometry()
         wmax = sg.width()
@@ -210,6 +273,14 @@ class MainWindow(QMainWindow):
             self._tree = builder.build(root=dir)
             for doc in self._tree:
                 self._docTree.addItem(doc)
+                self._reqStack.addDoc(doc)
+                self._reqView.addDoc(doc)
+
+    def _docSelected(self, item, col):
+        self._reqStack.makeDocActive(item.doc)
+        self._reqView.makeDocActive(item.doc)
+
+
 
 
 if __name__ == '__main__':
